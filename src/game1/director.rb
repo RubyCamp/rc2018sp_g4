b8321a8d53d824dbaa0750c61a50f1fd59f2abe4
Yuhei FUJITA <fujita@devinker.com>
2018-03-07 10:44:32 +0900

    update game1

--- src/game1/director.rb
+++ src/game1/director.rb
@@ -25,6 +25,7 @@ module Game1
 					end
 				when 1 #set rad
 					Window.draw_font(250, 280, "Push Button!!\n#{@degree}", @font)
+					@player.draw_lance(-@degree)
 					if @input.get_sw2 == 0
 						@degree += 1
 						@degree = 0 if @degree == 91
@@ -34,6 +35,8 @@ module Game1
 					end
 				when 2 #set initialize speed
 					Window.draw_font(250, 280, "Shout!!\nPower: #{@max_power}", @font)
+					@player.draw_lance(-@degree)
+					Window.draw_font(10, 550, "|" * (@input.get_sound / 10), @font)
 					if @cnt < 30
 						if frm_count(6) == 1
 							@cnt += 1
@@ -41,14 +44,14 @@ module Game1
 							@max_power = power if @max_power < power
 						end
 					else
-						@player.speed = @max_power / 5
+						@player.speed = @max_power / 10
 						p @player
 						@step += 1
 					end
 				when 3 #shot
 					Window.draw_font(250,280, "Go!!\nx: #{@player.x.to_i}y: #{@player.y.to_i}", @font)
 					@player.draw
-					# return @player.x if @player.y <= 0.0
+					return @player.x if @player.y <= 0.0
 			end
 
     	end
--- src/game1/player.rb
+++ src/game1/player.rb
@@ -1,34 +1,45 @@
 module Game1
 	class Player
-	  attr_accessor :x
-	  attr_accessor :y
-	  attr_accessor :speed
-	  attr_accessor :rad
+	  	attr_accessor :x
+	  	attr_accessor :y
+	  	attr_accessor :speed
+		attr_accessor :rad
 
-	  GRAVITY = 5.0
- 	 	def initialize
-	 	  @image = Image.load('images/player.png')
+		GRAVITY = 9.8
+		def initialize
+	  		@image = Image.load('images/lance.png', nil, nil, 20, 10)
 			@image.set_color_key(C_BLACK)
+			@image_width = 30
+			@image_height = 5
 			@x = 10
+			@dx = 10
 			@y = 500
+			@dy = 500
 			@speed = 0
 			@rad = 0
 			@time = 0
-		 end
+		end
 
 		def get_x
 			return @speed * @time * Math.cos(@rad)
 		end
 
 		def get_y
-				  return -0.5 * GRAVITY * @time * @time + @speed.to_f * Math.sin(@rad)
+				  return -0.5 * GRAVITY * @time * @time + @speed.to_f * @time * Math.sin(@rad)
+		end
+
+		def draw_lance(degree)
+				  Window.draw_rot(@x, @y, @image, degree, 0, @image.height / 2)
 		end
 
 		def draw
-			Window.draw(@x, @y, @image)
-			@time += 0.1
-			@y = 500 - get_y
-			@x = 10 + get_x
+			@time += 1.0/60.0
+			@dy = @y
+			@y = 500 - get_y * 5
+			@dx = @x
+			@x = 10 + get_x * 2
+			p degree = (Math.atan((@y - @dy) / (@x - @dx)))
+			Window.draw_rot(@x, @y, @image, -degree, @x, @y)
   		end
 	end
 end
