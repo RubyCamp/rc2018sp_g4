b1f1ca94f57f15166a495c1ea99a6fffdf4c1f14
dlstjq <dlstjq7685@gmail.com>
2018-03-07 09:05:58 +0900

    Update

--- src/game1/player.rb
+++ src/game1/player.rb
@@ -5,30 +5,30 @@ module Game1
 	  attr_accessor :speed
 	  attr_accessor :rad
 
-	  GRAVITY = 5.0
+	  GRAVITY = 9.8
  	 	def initialize
 	 	  @image = Image.load('images/player.png')
 			@image.set_color_key(C_BLACK)
 			@x = 10
-			@y = 500
+			@y = 10
 			@speed = 0
 			@rad = 0
 			@time = 0
-		 end
+		end
 
 		def get_x
 			return @speed * @time * Math.cos(@rad)
 		end
 
 		def get_y
-				  return -0.5 * GRAVITY * @time * @time + @speed.to_f * Math.sin(@rad)
+			return -(1/2) * GRAVITY * @time * @time + @speed * Math.sin(@rad)
 		end
 
 		def draw
 			Window.draw(@x, @y, @image)
-			@time += 0.1
-			@y = 500 - get_y
-			@x = 10 + get_x
+			@time += 1
+			@y = get_y
+			@x = get_x 
   		end
 	end
 end
