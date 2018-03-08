fdada45488afe8af76a7745d6de3b74eff0a433b
unknown <nomayeah0906@gmail.com>
2018-03-08 17:02:59 +0900

    update

--- src/scene_game/output.rb
+++ src/scene_game/output.rb
@@ -1,9 +1,11 @@
 class Out
     def initialize
         @image = Image.load('images/player.png')
+        @title = Image.load('images/title_mini.png')
         @back1 = Image.load('images/back1.png')
         @back2 = Image.load('images/back2.png')
         @font = Font.new(32, 'ＭＳ Ｐゴシック')
+        @font2 = Font.new(40, 'ＭＳ Ｐゴシック')
         @x = 240
         @y = 150
         @image.set_color_key(C_BLACK)
@@ -11,11 +13,13 @@ class Out
     end
 
     def draw
+        Window.draw(0,0,@title)
+        Window.draw_font(300, 80, "Game menu", @font2)
         Window.draw_font(300, 150, "Throwing Game", @font)
         Window.draw_font(300, 200, "Balance Game", @font)
         Window.draw_font(300, 250, "Timer Stop Game", @font)
         Window.draw_font(300, 300, "Rock-paper-scissors", @font)
-        Window.draw_font(300, 350, "終了", @font)
+        Window.draw_font(300, 350, "Exit", @font)
         Window.draw(@x, @y, @image)
     end
 
